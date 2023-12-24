761e469276bc134b9970f02fd47e8323c8faf7aa
Admin <Admin@ADMIN-PC>
2023-12-24 15:09:54 +0700

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Consumer;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+FanoutExchangeConsumer.Consume(channel);
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Producer;
+using System.Text;
+
+// Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+FanoutExchangePublisher.Publish(channel);
+
